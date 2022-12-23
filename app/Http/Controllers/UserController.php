070ca5abf5839186561ee73527a331aff2e60270
Enretro <envpogled@gmail.com>
2022-12-23 17:48:35 +0100

    Authentication Basic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,33 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout() {
+        auth()->logout();
+        return redirect('/')->with('success', 'En logged out.');
+    }
+
+    public function showCorrectHomepage() {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if  (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'En successfully logged in.');
+        } else {
+            return redirect('/')->with('failure', 'Invalid login.');
+        }
+    }
+
     public function register(Request $request) {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:30', Rule::unique('users', 'username')],
@@ -17,8 +44,9 @@ class UserController extends Controller
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return 'hello from register function';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'en is grateful to En creating an account.');
     }
 
 }
